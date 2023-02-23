ba853d28fc86efde160a8810fe8e62a8de2149b4
sameerkarjatkar <sameerkarjatkar@gmail.com>
2023-02-23 15:39:14 +0530

    Source Code Commit

---
+++ Plugins/BackUp/Source/BackUp/Private/UI/FRealBandBackUpUIManager.h
@@ -0,0 +1,64 @@
+//Copyright(C) 2022 VAXPORT all rights reserved.
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widgets/Docking/SDockTab.h"
+#include "Widgets/SCanvas.h"
+#include "Widgets/Input/SComboBox.h"
+#include "Widgets/Docking/SDockTab.h"
+#include "Widgets/Docking/SDockTab.h"
+
+#include "../../../../Engine\Plugins\Developer\Concert\ConcertMain\Source\Concert\Public\ConcertMessages.h"
+#include "SourceControl/Private/SourceControlModule.h"
+
+
+class IConcertSyncClient;
+class URealBandConfig;
+class SConcertSessionBrowser;
+class IConcertClientSession;
+//struct EConcertConnectionStatus;
+
+DECLARE_LOG_CATEGORY_EXTERN(LogManager, Log, All);
+
+
+
+class FRealBandBackUpUIManagerImpl : public TSharedFromThis<FRealBandBackUpUIManagerImpl>
+{
+
+private:
+
+	TSharedPtr<SWindow> pDialogMainWindow;
+	TSharedPtr<SOverlay> pOverlay;
+	TSharedPtr<SCanvas> pCanvas;
+	void SetupMenuItem();
+	void FillToolbar(FToolBarBuilder& ToolbarBuilder);
+	TSharedPtr<IConcertSyncClient> pConcertSyncClient;
+	URealBandConfig* pURealBandConfig;
+	FGuid ServerId;
+	TSharedPtr < SConcertSessionBrowser> ConcertSessionBrowser;
+	FSourceControlProjectDirDelegate ProjectDirHandle;
+	void GetPythonPath(FString& oPythonPath);
+public:
+
+	virtual ~FRealBandBackUpUIManagerImpl();
+	void Initialize();
+	void CreateWidgetWindow();
+	FString InitSourceVersionControl();
+	void InitMultiUserEditorControls();
+	void OnServersAssumedReady();
+	void HandleSyncSessionStartup(const IConcertSyncClient* SyncClient);
+	void HandleConcertSyncClientCreated(TSharedRef<IConcertSyncClient> Client);
+	void OnSessionConnectionChanged(IConcertClientSession& Session, EConcertConnectionStatus Status);
+	FReply JoinSession();
+	FReply Save();
+};
+
+
+class FRealBandBackUpUIManager
+{
+public:
+	static void Initialize();
+	//static bool DisplayStartWindow();
+	static TSharedPtr<FRealBandBackUpUIManagerImpl> Instance;
+	~FRealBandBackUpUIManager();
+};
---
+++ Source/RealBandMultiUserEditor.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class RealBandMultiUserEditorTarget : TargetRules
+{
+	public RealBandMultiUserEditorTarget( TargetInfo Target) : base(Target)
+	{
+        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+        Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.AddRange( new string[] { "RealBandMultiUser" } );
+	}
+}
