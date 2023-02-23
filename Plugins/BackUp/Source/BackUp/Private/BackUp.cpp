f8f7bb235c38769eb211c52ee740d7e763cd2f0d
sameerkarjatkar <sameerkarjatkar@gmail.com>
2023-02-23 10:25:20 +0530

    Initial Commit

---
+++ Plugins/BackUp/Source/BackUp/Private/BackUp.cpp
@@ -0,0 +1,110 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BackUp.h"
+#include "BackUpStyle.h"
+#include "BackUpCommands.h"
+#include "Misc/MessageDialog.h"
+#include "ToolMenus.h"
+#include "Http.h"
+
+#include "UI/FRealBandBackUpUIManager.h"
+
+static const FName BackUpTabName("BackUp");
+
+#define LOCTEXT_NAMESPACE "FBackUpModule"
+
+void FBackUpModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+	
+	FBackUpStyle::Initialize();
+	FBackUpStyle::ReloadTextures();
+
+	FBackUpCommands::Register();
+	
+	PluginCommands = MakeShareable(new FUICommandList);
+
+	PluginCommands->MapAction(
+		FBackUpCommands::Get().PluginAction,
+		FExecuteAction::CreateRaw(this, &FBackUpModule::PluginButtonClicked),
+		FCanExecuteAction());
+
+	UToolMenus::RegisterStartupCallback(FSimpleMulticastDelegate::FDelegate::CreateRaw(this, &FBackUpModule::RegisterMenus));
+}
+
+void FBackUpModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+
+	if (FRealBandBackUpUIManager::Instance.IsValid())
+	{
+		
+		FRealBandBackUpUIManager::Instance.Reset();
+	}
+
+	UToolMenus::UnRegisterStartupCallback(this);
+
+	UToolMenus::UnregisterOwner(this);
+
+	FBackUpStyle::Shutdown();
+
+	FBackUpCommands::Unregister();
+}
+
+void FBackUpModule::PluginButtonClicked()
+{
+	// Put your "OnButtonClicked" stuff here
+	FText DialogText = FText::Format(
+							LOCTEXT("PluginButtonDialogText", "Add code to {0} in {1} to override this button's actions"),
+							FText::FromString(TEXT("FBackUpModule::PluginButtonClicked()")),
+							FText::FromString(TEXT("BackUp.cpp"))
+					   );
+	//FMessageDialog::Open(EAppMsgType::Ok, DialogText);
+
+	FRealBandBackUpUIManager::Initialize();
+	//TSharedRef<IHttpRequest> Request = FHttpModule::Get().CreateRequest();
+	//Request->SetVerb("POST");
+	//Request->SetURL("https://example.com/backup");
+	//Request->SetHeader("Content-Type", "application/json");
+	//Request->SetContentAsString("{\"data\":\"example\"}");
+	//Request->OnProcessRequestComplete().BindLambda([](FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful) {
+	//	if (bWasSuccessful && Response.IsValid()) {
+	//		// Handle the response from the server
+	//	}
+	//	else {
+	//		FMessageDialog::Open(EAppMsgType::Ok, FText::FromString(L"Failed to connect"));
+	//		// Handle the error
+	//	}
+	//	});
+	//Request->ProcessRequest();
+}
+
+void FBackUpModule::RegisterMenus()
+{
+	// Owner will be used for cleanup in call to UToolMenus::UnregisterOwner
+	FToolMenuOwnerScoped OwnerScoped(this);
+
+	{
+		UToolMenu* Menu = UToolMenus::Get()->ExtendMenu("LevelEditor.MainMenu.Window");
+		{
+			FToolMenuSection& Section = Menu->FindOrAddSection("WindowLayout");
+			Section.AddMenuEntryWithCommandList(FBackUpCommands::Get().PluginAction, PluginCommands);
+		}
+	}
+
+	{
+		UToolMenu* ToolbarMenu = UToolMenus::Get()->ExtendMenu("LevelEditor.LevelEditorToolBar.PlayToolBar");
+		{
+			FToolMenuSection& Section = ToolbarMenu->FindOrAddSection("PluginTools");
+			{
+				FToolMenuEntry& Entry = Section.AddEntry(FToolMenuEntry::InitToolBarButton(FBackUpCommands::Get().PluginAction));
+				Entry.SetCommandList(PluginCommands);
+			}
+		}
+	}
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FBackUpModule, BackUp)
---
+++ Plugins/BackUp/Source/BackUp/Private/Config/RealBandConfig.h
@@ -0,0 +1,64 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EditorUtilityWidget.h"
+#include "CoreGlobals.h"
+#include "CoreMinimal.h"
+#include "ConcertMessageData.h"
+
+#include "RealBandConfig.generated.h"
+
+class IConcertSyncClient;
+/**
+ * 
+ */
+UCLASS(Config = Editor)
+class URealBandConfig : public UObject
+{
+	GENERATED_BODY()
+
+	FString	GetConcertArguments(const FString& ServerName, const FString& SessionName);
+	const FString& GetConcertServerName();
+	void OnServersAssumedReady();
+	void ConnectToSession();
+	const FString& GetConcertSessionName();
+	FString CachedConcertSessionName;
+	TSharedPtr<IConcertSyncClient> BackupClient;
+	void RegisterSettings();
+public:
+	virtual ~URealBandConfig();
+	enum class EConcertServerRequestStatus
+	{
+		None,
+		ShutdownRequested,
+		LaunchRequested,
+		ReuseExisting
+	};
+
+	EConcertServerRequestStatus ConcertServerRequestStatus = EConcertServerRequestStatus::None;
+
+	struct FLaunchMultiUserServerTrackingData
+	{
+		FString GeneratedMultiUserServerName;
+		FConcertServerInfo MultiUserServerInfo;
+		FProcHandle MultiUserServerHandle;
+	};
+
+
+	FLaunchMultiUserServerTrackingData ServerTrackingData;
+	bool FindOrLaunchConcertServer();
+	bool ConfigureUDPMessaging(FString& ConcatenatedArguments);
+	FString GetBackupServerName();
+	FString MakeSessionName();
+
+	URealBandConfig();
+//	void SetConfig(TSharedPtr<IConcertSyncClient> );
+	void ConfigureUDP();
+	void LaunchConcertServer(const FString & );
+	void FindAppropriateServer();
+	
+	TSharedPtr<IConcertSyncClient> GetBackupClient();
+	TSharedPtr<IConcertSyncClient> CreateConcertClient();
+};
