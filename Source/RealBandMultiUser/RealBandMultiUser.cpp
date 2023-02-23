2274c063d86e2c759646b08c7ca419ef8ea0a1da
sameerkarjatkar <sameerkarjatkar@gmail.com>
2023-02-23 14:36:17 +0530

    Cleanup

--- Source/RealBandMultiUser/RealBandMultiUser.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "RealBandMultiUser.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RealBandMultiUser, "RealBandMultiUser" );
